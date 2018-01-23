d719003b4dedbdfa5c1f81915c40c3f8d462f6bd
Zalan Farkas <milkshake.9601@gmail.com>
2018-01-23 15:33:46 +0000

    Update routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
+
+  root 'home_page#welcome'
   get 'home_page/welcome'
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
