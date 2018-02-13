75594f08ac69f9136fd0c585272b05ba1626db9f
Senpai <senpai@google.com>
2018-02-13 15:24:42 +0000

    Fix student id

--- app/controllers/api_controller.rb
+++ app/controllers/api_controller.rb
@@ -69,7 +69,7 @@ class ApiController < ApplicationController
       end
       
       # Check is student is enrolled for the course
-      if Enrolment.where(["student_id = ? and CAST(course_id AS BIGINT) = ?", student.id, course.id]).nil?
+      if Enrolment.where(["student_id = ? and course_id = ?", student.id, course.id]).nil?
         return render_json_error("Student is not enrolled for course: #{course_id}")
       end
       
@@ -83,7 +83,7 @@ class ApiController < ApplicationController
       end
       Attendance.create(student_id: student.id, practical_id: practicals.first.id)
       
-      render :json => { :success => true, :student_id => sam_student_id }
+      render :json => { :success => true, :student_id => student.sam_student_id }
     end
     
   end
