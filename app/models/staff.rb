fb7371a1a620d4ba12f6b44382049b8424147300
Senpai <senpai@google.com>
2018-02-20 22:15:44 +0000

    setting bootstrap up + enhancing layout of login view

--- app/models/staff.rb
+++ app/models/staff.rb
@@ -1,8 +1,8 @@
 class Staff < ApplicationRecord
   # Include default devise modules. Others available are:
-  # :confirmable, :timeoutable and :omniauthable, :registerable,:rememberable,
+  # :confirmable, :timeoutable and :omniauthable, :registerable,
   devise :database_authenticatable, :lockable,
-         :recoverable, :trackable, :validatable, :timeoutable
+         :recoverable, :trackable, :validatable, :timeoutable, :rememberable
          
   has_many :courses
   validates :sam_staff_id, presence: true, uniqueness: true
--- app/models/student.rb
+++ app/models/student.rb
@@ -1,7 +1,7 @@
 class Student < ApplicationRecord
   # Include default devise modules. Others available are:
-  # :confirmable, :lockable, :timeoutable and :omniauthable
-  devise :database_authenticatable, :registerable,
+  # :confirmable, :lockable, :timeoutable and :omniauthable, :registerable
+  devise :database_authenticatable, :lockable, :timeoutable,
          :recoverable, :rememberable, :trackable, :validatable
          
   has_many :attendances
